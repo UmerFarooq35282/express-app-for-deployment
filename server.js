25ae517821d820753b24560e44015053a4a66b3c
Umer Farooq <umerfarooq2821035@gmail.com>
2025-08-04 21:06:45 +0500

    express app for deployment practice

---
+++ server.js
@@ -0,0 +1,13 @@
+import "dotenv/config";
+import express from "express";
+
+const PORT = process.env.PORT || 3000;
+const app = express();
+
+app.get("/" , (req,res) => {
+    res.send("This is Home Page")
+})
+
+app.listen(PORT, () => {
+  console.log(`App is listening on PORT: ${PORT}`);
+});
